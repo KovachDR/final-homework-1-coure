fad410f7f981a22734c6ea963a4d8fa8d0a97ec0
Brein <Brein_02_93@mail.ru>
2023-04-04 00:50:38 +0300

    Черновой вариант выполненного задания базового уровня.

--- src/baseDifficulty/Main.java
+++ src/baseDifficulty/Main.java
@@ -1,8 +1,9 @@
 package baseDifficulty;
 
 public class Main {
+    static Employee[] employees = new Employee[10];
+
     public static void main(String[] args) {
-        Employee[] employees = new Employee[10];
         employees[0] = new Employee("Иванов Иван Иванович", 1, 50000);
         employees[1] = new Employee("Сергеев Сергей Сергеевич", 2, 55000);
         employees[2] = new Employee("Петров Пётр Петрович", 3, 60000);
@@ -14,17 +15,70 @@ public class Main {
         employees[8] = new Employee("Максимов Максим Максимович", 4, 55000);
         employees[9] = new Employee("Антонов Антон Антонович", 5, 65000);
 
+        printInfo();
+
+//        for (Employee employee : employees) {
+//            System.out.println(employee);
+//        }
+
+        calculationTotalSalaryAndAverageSalary();
+//        int totalSalaryInMonth = 0;
+//        for (Employee employee : employees) {
+//            totalSalaryInMonth = totalSalaryInMonth + employee.getSalary();
+//        }
+//        System.out.println("Сумма затрат на зарплаты в месяц составляет " + totalSalaryInMonth);
+
+        checkMinSalary();
+//        int minSalary = employees[0].getSalary();
+//        for (int i = 0; i < employees.length; i++) {
+//            if (employees[i].getSalary() < minSalary) {
+//                minSalary = employees[i].getSalary();
+//            }
+//        }
+//        for (int i = 0; i < employees.length; i++) {
+//            if (minSalary == employees[i].getSalary()) {
+//                System.out.println("Минимальную зарплату получает " + employees[i]);
+//            }
+//        }
+
+        checkMaxSalary();
+//        int maxSalary = employees[0].getSalary();
+//        for (int i = 0; i < employees.length; i++) {
+//            if (employees[i].getSalary() > maxSalary) {
+//                maxSalary = employees[i].getSalary();
+//            }
+//        }
+//        for (int i = 0; i < employees.length; i++) {
+//            if (maxSalary == employees[i].getSalary()) {
+//                System.out.println("Максимальную зарплату получает " + employees[i]);
+//            }
+//        }
+
+//        double averageSalary = calculationTotalSalary() / employees.length;
+//        System.out.println(averageSalary);
+
+
+
+getNamesOfEmployees();
+        //        for (Employee employee : employees){
+//            System.out.println(employee.getName());
+//        }
+    }
+    public static void printInfo(){
         for (Employee employee : employees) {
             System.out.println(employee);
         }
-        System.out.println("///////////////////////////////////////");
+    }
+    public static void calculationTotalSalaryAndAverageSalary() {
         int totalSalaryInMonth = 0;
         for (Employee employee : employees) {
             totalSalaryInMonth = totalSalaryInMonth + employee.getSalary();
         }
-        System.out.println("Сумма затрат на зарплаты в месяц составляет " + totalSalaryInMonth);
-        System.out.println("///////////////////////////////////////");
-
+        System.out.println("Сумма затрат на зарплаты в месяц составляет - " + totalSalaryInMonth);
+        double averageSalary = (double) totalSalaryInMonth / employees.length;
+        System.out.println("Средняя зарплата составляет - " + averageSalary);
+    }
+    public static void checkMinSalary () {
         int minSalary = employees[0].getSalary();
         for (int i = 0; i < employees.length; i++) {
             if (employees[i].getSalary() < minSalary) {
@@ -36,7 +90,8 @@ public class Main {
                 System.out.println("Минимальную зарплату получает " + employees[i]);
             }
         }
-        System.out.println("///////////////////////////////////////");
+    }
+    public static void checkMaxSalary() {
         int maxSalary = employees[0].getSalary();
         for (int i = 0; i < employees.length; i++) {
             if (employees[i].getSalary() > maxSalary) {
@@ -48,15 +103,15 @@ public class Main {
                 System.out.println("Максимальную зарплату получает " + employees[i]);
             }
         }
-        System.out.println("///////////////////////////////////////");
-        double averageSalary = totalSalaryInMonth / employees.length;
-        System.out.println(averageSalary);
+    }
 
-        System.out.println("///////////////////////////////////////");
+    public static void getNamesOfEmployees() {
         for (Employee employee : employees){
             System.out.println(employee.getName());
         }
     }
-
+    public static void delimiter() {
+        System.out.println("////////////////////////////////////////////////////");
+    }
 }
 
